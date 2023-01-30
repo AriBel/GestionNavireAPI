947390bbcfc613468519312e4619689789d6609c
Hugues Aristide Belem <abelem@morneaushepell.com>
2023-01-29 19:16:49 -0500

    initial commit

---
+++ Migrations/SqlServerMigrations/20230129062138_AddVesselEntity.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations.SqlServerMigrations
+{
+    public partial class AddVesselEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vessels",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Length = table.Column<int>(type: "int", nullable: false),
+                    Width = table.Column<int>(type: "int", nullable: false),
+                    NetWeight = table.Column<int>(type: "int", nullable: false),
+                    GrossWeight = table.Column<int>(type: "int", nullable: false),
+                    Info = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vessels", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vessels");
+        }
+    }
+}
---
+++ Migrations/SqlServerMigrations/20230129183416_ModifyVesselEntityFields.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations.SqlServerMigrations
+{
+    public partial class ModifyVesselEntityFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Year",
+                table: "Vessels",
+                newName: "YearOfConstruction");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Vessels",
+                newName: "VesselNumber");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Width",
+                table: "Vessels",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Length",
+                table: "Vessels",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "YearOfConstruction",
+                table: "Vessels",
+                newName: "Year");
+
+            migrationBuilder.RenameColumn(
+                name: "VesselNumber",
+                table: "Vessels",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Width",
+                table: "Vessels",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Length",
+                table: "Vessels",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
